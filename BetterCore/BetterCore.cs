80ebe43ae591bd77a294dd54a6709c69bc5dfa05
Shadow32055 <shadow32055@gmail.com>
2024-01-04 14:47:54 -0500

    Cleanup & Standardize
    Removed un-need dependencies (Harmony, etc.)
    Cleaned up SubModule.xml
    Removed common modName
    Converted Logger to NotifyHelper to prevent TW Logger conflict.
    Broke SendMessage into ReportError and ChatMessage to better Identify there purpose.
    Updated SubModule.cs to BetterCore.cs

---
+++ BetterCore/BetterCore.cs
@@ -0,0 +1,41 @@
+﻿using BetterCore.Utils;
+using System;
+using TaleWorlds.MountAndBlade;
+
+namespace BetterCore {
+    public class BetterCore : MBSubModuleBase {
+
+		public static string modName { get; private set; } = "ForgotToSet";
+
+		private bool _isInitialized = false;
+        private bool _isLoaded = false;
+
+		protected override void OnSubModuleLoad() {
+			try {
+                if (_isInitialized)
+                    return;
+
+                modName = base.GetType().Assembly.GetName().Name;
+
+                _isInitialized = true;
+            } catch (Exception e) {
+                NotifyHelper.ReportError(modName, "OnSubModuleLoad threw exception " + e);
+            }
+		}
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot() {
+			base.OnBeforeInitialModuleScreenSetAsRoot();
+			try {
+
+                if (_isLoaded)
+                    return;
+
+                NotifyHelper.ChatMessage(modName + " Loaded.", MsgType.Good);
+
+                _isLoaded = true;
+			} catch (Exception e) {
+                NotifyHelper.ReportError(modName, "OnBeforeInitialModuleScreenSetAsRoot threw exception " + e);
+            }
+		}
+    }
+}
---
+++ BetterCore/Utils/NotifyHelper.cs
@@ -0,0 +1,59 @@
+﻿using TaleWorlds.Library;
+
+namespace BetterCore.Utils {
+    public class NotifyHelper {
+
+        public static void ReportError(string mod, string text) {
+            string message = mod + ": " + text;
+            PrintToLog(message);
+            ChatMessage(message, MsgType.Warning);
+        }
+
+        public static void ChatMessage(string text, MsgType type) {
+            Color color = Colors.Gray;
+
+            switch (type) {
+                case MsgType.Normal:
+                    color = Colors.White;
+                    break;
+                case MsgType.Notify:
+                    color = Colors.Cyan;
+                    break;
+                case MsgType.Risk:
+                    color = Colors.Yellow;
+                    break;
+                case MsgType.Alert:
+                    color = Colors.Magenta;
+                    break;
+                case MsgType.Warning:
+                    color = Colors.Red;
+                    break;
+                case MsgType.Good:
+                    color = Colors.Green;
+                    break;
+            }
+
+            PrintToChat(text, color);
+        }
+
+        public static void PrintToChat(string text, Color color) {
+            InformationManager.DisplayMessage(new InformationMessage(text, color));
+            //ToDo - Add custom colors
+            //InformationManager.DisplayMessage(new InformationMessage(s, color ?? new Color(1f, 0f, 0f)));
+        }
+
+        public static void PrintToLog(string text) {
+            Debug.Print(text);
+        }
+    }
+
+    public enum MsgType {
+        None,//Gray
+        Normal,//White
+        Notify,//Cyan
+        Risk,//Yellow
+        Alert,//Magenta
+        Warning,//Red
+        Good //Green
+    }
+}
